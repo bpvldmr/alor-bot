9a25a222f253ea19dbaf14dffd896ba70cc23f50
bpvldmr <bpvldmr@gmail.com>
2025-06-17 22:56:54 +0300

    auth.py

--- auth.py
+++ auth.py
@@ -1,12 +1,15 @@
-
 import requests
+from loguru import logger
 
 REFRESH_TOKEN = "a0facbb9-aadd-4f67-88e0-1204dcd392b5"
 CLIENT_ID = "93e214f3a9e74524a075"
 CLIENT_SECRET = "TmAUmTAz6JJbhLZZguRzoP3p5dJ9RUrvlAzz19Y9U0U="
 REDIRECT_URI = "https://oauth.alor.ru/blank.html"
 
+ACCESS_TOKEN = None
+
 def get_access_token():
+    global ACCESS_TOKEN
     url = "https://oauth.alor.ru/refresh"
     payload = {
         "refresh_token": REFRESH_TOKEN,
@@ -16,9 +19,16 @@ def get_access_token():
         "grant_type": "refresh_token"
     }
 
-    response = requests.post(url, data=payload)
-    if response.status_code == 200:
-        return response.json().get("access_token")
-    else:
-        print("❌ Ошибка получения токена:", response.text)
-        return None
+    try:
+        response = requests.post(url, data=payload)
+        if response.status_code == 200:
+            ACCESS_TOKEN = response.json().get("access_token")
+            logger.info("✅ Access token успешно обновлён.")
+        else:
+            logger.error(f"❌ Ошибка получения токена: {response.text}")
+            ACCESS_TOKEN = None
+    except Exception as e:
+        logger.exception(f"⚠️ Ошибка соединения: {e}")
+        ACCESS_TOKEN = None
+
+    return ACCESS_TOKEN
