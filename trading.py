0582adc36714761c44d274eeee5999a8554c327f
bpvldmr <bpvldmr@gmail.com>
2025-06-22 23:28:50 +0300

    trading.py

--- trading.py
+++ trading.py
@@ -20,13 +20,12 @@ def get_current_balance():
     try:
         access_token = get_access_token()
         headers = {"Authorization": f"Bearer {access_token}"}
-        url = f"https://api.alor.ru/md/v2/Clients/{ACCOUNT_ID}/summary"
+        url = f"https://api.alor.ru/trade/v2/clients/{ACCOUNT_ID}/summary?exchange=FORTS"
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         data = response.json()
 
         return float(data.get("portfolio", {}).get("equity", 0))
-
     except Exception as e:
         print(f"Ошибка получения баланса: {e}")
         return 0
