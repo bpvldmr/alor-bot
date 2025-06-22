129700345effbec753c478f76a120e0d47ad6689
bpvldmr <bpvldmr@gmail.com>
2025-06-22 14:12:51 +0300

    config.py

--- config.py
+++ config.py
@@ -1,16 +1,28 @@
+# config.py
+
+# Соответствие тикеров TradingView и тикеров Alor
 TICKER_MAP = {
     "MOEX:CRU2025": {"trade": "CRU5"},
     "MOEX:NGN2025": {"trade": "NGN5"}
 }
 
-MAX_QTY = {
-    "CRU5": 140,
-    "NGN5": 20
+# Начальное количество контрактов
+START_QTY = {
+    "CRU5": 5,
+    "NGN5": 3
 }
 
-ADD_QTY = {
-    "CRU5": 20,
+# Максимальное количество контрактов
+MAX_QTY = {
+    "CRU5": 9,
     "NGN5": 5
 }
 
+# Количество добавки при усреднении
+ADD_QTY = {
+    "CRU5": 2,
+    "NGN5": 1
+}
+
+# Твой ID аккаунта на ALOR
 ACCOUNT_ID = "7502QAB"
