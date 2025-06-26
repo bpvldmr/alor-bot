41d32e2fb78312b7e251a0edac236c98d0736509
bpvldmr <bpvldmr@gmail.com>
2025-06-26 15:27:27 +0300

    scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,15 @@
-# scheduler.py
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from auth import get_access_token
 from balance import send_balance_to_telegram
 import httpx
 from loguru import logger
+from trading import enable_trading, disable_trading
 
 BASE_URL = "https://api.alor.ru"
 ACCOUNT_ID = "7502QAB"
 
-# 🕒 Задача: запрос баланса и отправка в Telegram
+# 📊 Задача: запрос баланса и отправка в Telegram
 async def scheduled_balance_job():
     token = get_access_token()
     url = f"{BASE_URL}/md/v2/Clients/MOEX/{ACCOUNT_ID}/summary"
@@ -28,7 +28,20 @@ async def scheduled_balance_job():
     except Exception as e:
         logger.exception("❌ Ошибка при автоотправке баланса в Telegram")
 
+# ⏹️ Отключение торговли
+async def scheduled_disable_trading():
+    await disable_trading()
+
+# ▶️ Включение торговли
+async def scheduled_enable_trading():
+    await enable_trading()
+
 # ⏱ Планировщик задач
 scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
-scheduler.add_job(scheduled_balance_job, CronTrigger(hour=11, minute=0), id="morning_balance")
-scheduler.add_job(scheduled_balance_job, CronTrigger(hour=18, minute=0), id="evening_balance")
+
+# Отправка баланса
+scheduler.add_job(scheduled_balance_job, CronTrigger(hour=11, minute=0, day_of_week='mon-fri'), id="morning_balance")
+scheduler.add_job(scheduled_balance_job, CronTrigger(hour=18, minute=0, day_of_week='mon-fri'), id="evening_balance")
+
+# Включение/отключение торговли
+schedu
