afd77442f8445a2e2a8ebea77ee62cf354893c48
bpvldmr <bpvldmr@gmail.com>
2025-06-27 16:34:47 +0300

    webhook.py

--- webhook.py
+++ webhook.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Request
 import json
 from telegram_logger import send_telegram_log
-from trading import handle_trading_signal
+from trading import handle_trading_signal as process_signal
+
 
 router = APIRouter()
 
